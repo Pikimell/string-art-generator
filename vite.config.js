7e153bd32e4ed051e905dc4771ca498740de1fae
Pashchenko Volodymyr <pikimelposhta@gmail.com>
2024-08-03 01:41:15 +0300

    update files

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import glob from 'glob';
 import injectHTML from 'vite-plugin-html-inject';
 import FullReload from 'vite-plugin-full-reload';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 export default defineConfig(({ command }) => {
   return {
@@ -25,6 +26,17 @@ export default defineConfig(({ command }) => {
       },
       outDir: '../dist',
     },
-    plugins: [injectHTML(), FullReload(['./src/**/**.html'])],
+    plugins: [
+      injectHTML(),
+      FullReload(['./src/**/**.html']),
+      viteStaticCopy({
+        targets: [
+          {
+            src: 'js/**',
+            dest: 'js',
+          },
+        ],
+      }),
+    ],
   };
 });
